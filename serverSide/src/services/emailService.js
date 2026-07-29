e81f33d786f821f165e24f95ec10d539e80b7e05
SAKETH070706 <lgksaketh@gmail.com>
2026-07-30 00:55:04 +0530

    changed transporter port

--- serverSide/src/services/emailService.js
+++ serverSide/src/services/emailService.js
@@ -2,12 +2,16 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 
+import nodemailer from "nodemailer";
+
 const transporter = nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
-    }
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // STARTTLS
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 export const sendOTPEmail = async (email,otp)=>{
