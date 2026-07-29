8b6c6d41ac09926b636fe9b6272682cd48eb9d74
SAKETH070706 <lgksaketh@gmail.com>
2026-07-30 00:27:23 +0530

    Use environment variables for API and Socket URLs

--- clientSide/src/services/roomApi.js
+++ clientSide/src/services/roomApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/rooms",
+  baseURL: `${import.meta.env.VITE_API_URL}/rooms`,
 });
 
 API.interceptors.request.use((config) => {
